944d241eff251c4ebd6ee95a69d5789abf74d4a9
武内駿 <takeuchishun@takeuchishun-no-MacBook-Pro.local>
2015-06-01 03:27:10 +0900

    Initial Commit

---
+++ phyandcoltest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  phyandcoltest
+//
+//  Created by 武内駿 on 2015/06/01.
+//  Copyright (c) 2015年 Syun Takeuchi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
